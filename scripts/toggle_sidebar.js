d765bf45936eab33f9a8aa0f597c869d711abcbd
Pavel Vesnin <vesninpavel@gmail.com>
2025-04-10 10:06:54 +0700

    ю

--- scripts/toggle_sidebar.js
+++ scripts/toggle_sidebar.js
@@ -1,6 +1,10 @@
-function toggleSidebar() {
+﻿function toggleSidebar() {
     const sidebar = document.getElementById('sidebar');
     const container = document.querySelector('.container');
+    const tab = document.getElementById('toggle-tab');
+
     sidebar.classList.toggle('hidden');
     container.classList.toggle('fullwidth');
+    tab.classList.toggle('closed');
+    tab.innerText = tab.classList.contains('closed') ? '❯ Info' : '❮ Info';
 }
